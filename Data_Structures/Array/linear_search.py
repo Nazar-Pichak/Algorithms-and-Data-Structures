a9d66ff692552bb9736c1393b3fa61acdfc494e2
pichak_NAZARIY1996 <pichak.nazariy@gmail.com>
2023-09-09 23:30:31 +0200

    created new file

---
+++ Data_Structures/Array/linear_search.py
@@ -0,0 +1,10 @@
+def linear_search(arr, item):
+    for i in range(len(arr)):
+        if item == arr[i]:
+            return f"The item {item} was found at index {i}"
+    return f"The item {item} is not presented in array"
+    
+if __name__ == '__main__':
+    item = input("Enter your item: ")
+    arr = list(input("Enter your array: "))
+    print(linear_search(arr, item))
