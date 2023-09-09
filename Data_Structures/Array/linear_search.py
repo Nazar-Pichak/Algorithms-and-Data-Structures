a8bfb879f69b410b4c887a33cfab62d5425a7d99
pichak_NAZARIY1996 <pichak.nazariy@gmail.com>
2023-09-10 00:15:48 +0200

    apdated file

--- Data_Structures/Array/linear_search.py
+++ Data_Structures/Array/linear_search.py
@@ -1,9 +1,12 @@
+# The Linear search algorithm in the Array Data Structure
+
 def linear_search(arr, item):
     for i in range(len(arr)):
         if item == arr[i]:
             return f"The item {item} was found at index {i}"
     return f"The item {item} is not presented in array"
-    
+
+                    
 if __name__ == '__main__':
     item = input("Enter your item: ")
     arr = list(input("Enter your array: "))
