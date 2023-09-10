ac0777b4b05eb012b17bc07837d391c3d8b4e518
pichak_NAZARIY1996 <pichak.nazariy@gmail.com>
2023-09-10 02:44:41 +0200

    apdated file

--- Data_Structures/Array/sentinel_search.py
+++ Data_Structures/Array/sentinel_search.py
@@ -1,6 +1,6 @@
 # The Sentinel search in Array Data Structure
 
-def sentinel_search(arr, n, item):
+def sentinel_search(arr: list, n: int, item: str):
     last = arr[n - 1]
     arr[n - 1] = item
     i = 0
@@ -13,7 +13,7 @@ def sentinel_search(arr, n, item):
         return f"The item {item} was found at index {i}"
     return f'The item {item} is not presented in array'
 
-item = 23
-arr = [2, 4, 5, 6, 8, 2, 4, 5, 23]
+item = input("Enter your item: ")
+arr = list(input('Enter your array: '))
 n = len(arr)
 print(sentinel_search(arr, n, item))
